ab35ae602547984f0e7eb6aa7b5f51de89222bbc
SwarnimBhattad <swarnim@mohrstade.de>
2026-05-05 12:26:09 +0200

    Add files via upload

--- present-live-integration-app-sample/webpack.development.js
+++ present-live-integration-app-sample/webpack.development.js
@@ -10,7 +10,9 @@ module.exports = () => ({
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+      "Cross-Origin-Opener-Policy": "unsafe-none",
+      "Cross-Origin-Embedder-Policy": "unsafe-none"
     },
     port: PORT,
   },
